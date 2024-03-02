aa9ee71b57c12493bff53f08abac45403a8f9e56
mattD <matthieu.l.dubray@gmail.com>
2024-03-02 13:08:10 -0800

    packet sniffer using flask apis to send packets back to user.

--- app.py
+++ app.py
@@ -1,25 +1,33 @@
-from flask import Flask
+import requests
+from flask import Flask, jsonify, request
+from packetSniffer import pkt_sniffer, set_active, get_packets, clear_packets, set_filters
+from threading import Thread
+from time import sleep
 
-from packetSniffer import *
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-
-    return 'Hello World!'
-
-
-@app.route('/start')
-def start_sniff_api():  # put application's code here
-    start_scan()
+@app.route('/start', methods=['POST'])
+def start_sniff_api():
+    data = request.get_json()
+    set_filters(data)
+    sleep(1)
+    set_active(True)
+    Thread(target=pkt_sniffer).start()
     return 'started'
 
 
 @app.route('/stop')
-def stop_sniff_api():  # put application's code here
-    stop_scan()
+def stop_sniff_api():
+    set_active(False)
     return 'stopped'
+
+
+@app.route('/get_packets')
+def return_packets():
+    packets = jsonify(get_packets())
+    clear_packets()
+    return packets
 
 
 if __name__ == '__main__':
--- packetSniffer.py
+++ packetSniffer.py
@@ -1,19 +1,75 @@
 from scapy.all import *
+from scapy.layers.inet import IP, TCP, UDP
+
+packets = []
+isActive = False
+filter_string = ''
 
 
-def print_pkt(pkt):
-    try:
-        print(pkt.show(dump=True))
-    except UnicodeEncodeError:
-        print("Character encoding error")
+def pkt_sniffer():
+    global isActive
+    global packets
+
+    def print_pkt(pkt):
+        try:
+            print(pkt)
+            if pkt is not None and IP in pkt:
+                sniffed_pkt = {'src_ip': pkt[IP].src,
+                               'dst_ip': pkt[IP].dst,
+                               'protocol': pkt[IP].proto}
+                if TCP in pkt:
+                    print("TCP")
+                    sniffed_pkt['src_port'] = pkt[TCP].sport
+                    sniffed_pkt['dst_port'] = pkt[TCP].dport
+                    payload_data = pkt[TCP].payload
+                elif UDP in pkt:
+                    print("UDP")
+                    sniffed_pkt['src_port'] = pkt[UDP].sport
+                    sniffed_pkt['dst_port'] = pkt[UDP].dport
+                    payload_data = pkt[UDP].payload
+                else:
+                    payload_data = None
+
+                if payload_data:
+                    sniffed_pkt['payload'] = payload_data.load.decode('UTF-8', 'ignore')
+                packets.append(sniffed_pkt)
+        except UnicodeEncodeError:
+            print("Character encoding error")
+
+    while isActive:
+        # method to be able to stop and start sniffer as user requires
+        sniff(count=10, prn=print_pkt, filter=filter_string)
 
 
-t = AsyncSniffer(prn=print_pkt)
+# set whether sniffer is active or not
+def set_active(active):
+    global isActive
+    isActive = active
 
 
-def start_scan():
-    t.start()
+# retrieve packets sniffed
+def get_packets():
+    global packets
+    return packets.copy()
 
 
-def stop_scan():
-    t.stop()
+# when user receives packets already sniffed, dont want to resend old packets again
+def clear_packets():
+    global packets
+    packets = []
+
+
+# https://biot.com/capstats/bpf.html bpf filters used in scapy
+def set_filters(data):
+    global filter_string
+    for key, value in data.items():
+        if key == 'protocol':
+            filter_string += value
+        elif key == 'dst_ip':
+            filter_string += ' and dst host ' + value
+        elif key == 'dst_port':
+            filter_string += ' and dst port ' + value
+        elif key == 'src_ip':
+            filter_string += ' and src host ' + value
+        elif key == 'src_port':
+            filter_string += ' and src port ' + value
